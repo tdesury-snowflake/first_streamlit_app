9f97b2eff8035863256b27ce88821e286762f9d3
tdesury-snowflake <130046119+tdesury-snowflake@users.noreply.github.com>
2023-04-06 11:20:03 +0200

    Update streamlit_app.py
    Change Header
    Add Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omerga3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+
+streamlit.text('🥣 Omerga3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
